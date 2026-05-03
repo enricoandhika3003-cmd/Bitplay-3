d2032a4da1d5ebc841276d9c31f6fc1054ffa8bf
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2026-05-03 16:06:36 +0700

    done 2

--- demo.py
+++ demo.py
@@ -55,3 +55,22 @@ print(f"Result of {a} / {b} is {divide(a, b)}")
 #Input: a=72, b=3
 
 #Output: 24
+
+#Longest1's
+def con1(n):
+    binary = bin(n)[2:]  # Convert to binary and remove '0b'
+    max_count = 0
+    current_count = 0
+
+    for bit in binary:
+        if bit == '1':
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 0
+
+    return max_count
+
+num = int(input("Enter your number: "))
+result = con1(num)
+print("Longest consecutive 1's length: ", result)
